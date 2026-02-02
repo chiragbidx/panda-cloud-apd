a887017ec338a7877e34ac95f74d771007ae302a
chiragbidx <chirag@bidx.ai>
2026-02-02 08:00:36 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // ✅ Replacement for experimental.ppr
   cacheComponents: true,
+  allowedDevOrigins: [
+    'https://panda-cloud-apd-production.up.railway.app'
+  ]
 };
 
 export default nextConfig;
